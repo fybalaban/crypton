344a479082fa0340a46c3cbfd44ab5cef6c2f502
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2022-02-12 22:00:16 +0300

    Implement unit test for GetRandomBytes()

---
+++ cryptonTests/KeyGenerationTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+namespace crypton.Tests
+{
+    [TestClass()]
+    public class KeyGenerationTests
+    {
+        [TestMethod()]
+        public void GetRandomBytesTest()
+        {
+            try
+            {
+                KeyGeneration.GetRandomBytes(0);
+            }
+            catch (Exception exception)
+            {
+                Assert.IsTrue(exception is ArgumentOutOfRangeException);
+            }
+
+            try
+            {
+                KeyGeneration.GetRandomBytes(-1);
+            }
+            catch (Exception exception)
+            {
+                Assert.IsTrue(exception is ArgumentOutOfRangeException);
+            }
+
+            Assert.AreEqual(5, KeyGeneration.GetRandomBytes(5).Length);
+        }
+    }
+}
